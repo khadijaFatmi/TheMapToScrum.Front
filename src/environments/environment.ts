6f021b186d0a5a692564d5e76911ee8762946818
khadijaFatmi <khadijafatmi@protonmail.com>
2020-09-22 19:38:13 +0200

    pipeline test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api'
-  //apiUrl: 'http://dev.themaptoscrum.net/api'
+  // apiUrl: 'http://localhost:5000/api'
+  apiUrl: 'http://dev.themaptoscrum.net/api'
 };
 
 
